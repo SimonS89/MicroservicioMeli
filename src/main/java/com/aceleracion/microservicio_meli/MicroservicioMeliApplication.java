b61858cf4e4dd897e4fc7a11042fec4b1d30d130
SimonS89 <simonrsueldo@gmail.com>
2023-08-25 12:04:39 -0300

    first commit

---
+++ src/main/java/com/aceleracion/microservicio_meli/MicroservicioMeliApplication.java
@@ -0,0 +1,13 @@
+package com.aceleracion.microservicio_meli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicioMeliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicioMeliApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aceleracion/microservicio_meli/MicroservicioMeliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aceleracion.microservicio_meli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioMeliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
