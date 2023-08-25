ffaa05b19a352861ce597e5ed0ffb537db2b4a79
SimonS89 <simonrsueldo@gmail.com>
2023-08-25 13:25:17 -0300

    add first project structure meli

---
+++ product-service/src/main/java/com/aceleracion/sub/productservice/ProductServiceApplication.java
@@ -0,0 +1,13 @@
+package com.aceleracion.sub.productservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductServiceApplication.class, args);
+	}
+
+}
---
+++ vehicles-service/src/main/java/com/aceleracion/sub/vehiclesservice/VehiclesServiceApplication.java
@@ -0,0 +1,13 @@
+package com.aceleracion.sub.vehiclesservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehiclesServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehiclesServiceApplication.class, args);
+	}
+
+}
